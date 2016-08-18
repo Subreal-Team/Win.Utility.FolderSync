f2b1e67b093f23abfadff94a71c736bb8c6869be
SnowLeo <alex.v.timofeev@gmail.com>
2016-08-18 15:56:35 +0500

    ConfigurationBase вынесен в отдельный файл

--- UtilityConfiguration.cs
+++ UtilityConfiguration.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Reflection;
-using System.Text.RegularExpressions;
-
-namespace FolderSync
+﻿namespace FolderSync
 {
 	public class UtilityConfiguration : ConfigurationBase
     {
@@ -19,42 +14,4 @@ namespace FolderSync
 		[CommandLineArgument("SourceFolder")]
 		public string SourceFolder { get; protected set; }
     }
-
-	public abstract class ConfigurationBase
-	{
-		protected string[] _arguments = Environment.GetCommandLineArgs();
-
-		protected ConfigurationBase()
-        {
-			// по всем публичным свойствам класса
-            var typeInfo = this.GetType();
-            var publicProps = typeInfo.GetProperties(BindingFlags.Public | BindingFlags.Instance);
-
-			// взять свойства с атрибутом CommandLineArgument
-			var attributedProps = publicProps.Select(x => new { property = x, customAttributes = x.GetCustomAttributes() })
-				.Where(x => x.customAttributes.Any(y => y.GetType() == typeof(CommandLineArgumentAttribute)))
-				.ToList();
-
-			foreach (var prop in attributedProps)
-            {
-	            foreach (var attr in prop.customAttributes)
-	            {
-		            var cmdAttr = (CommandLineArgumentAttribute) attr;
-		            var cmdValue = _arguments.FirstOrDefault(x => x.ToUpper().Contains(cmdAttr.Name.ToUpper()));
-		            if (String.IsNullOrWhiteSpace(cmdValue)) continue;
-
-		            var match = Regex.Match(cmdAttr.ParseTemplate, "{name}(.*){value}");
-		            if (!match.Success || match.Groups.Count <= 0) continue;
-
-		            var splitter = match.Groups[1].Value;
-		            var value = Regex.Split(cmdValue, splitter);
-		            prop.property.SetValue(this,
-			            (value.Length == 2) && !String.IsNullOrWhiteSpace(value[1])
-				            ? value[1]
-				            : cmdAttr.DefaultValue);
-
-	            }
-            }
-        }
-	}
 }
